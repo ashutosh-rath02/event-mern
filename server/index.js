72ef6f5804db18fad9e1bc13b38beb0d043a9eab
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-05-22 15:56:10 +0530

    hopes #1

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ const corsOptions = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
