063489b1d75161c446ab839551d9488860efbe40
Niraj Yadav <yadavniraj898@gmail.com>
2024-05-24 00:04:52 +0530

    Removed proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://ucla-website.onrender.com",
-        changeOrigin: true,
-      },
-    },
   },
 });
